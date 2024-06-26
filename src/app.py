8564d1d896e74148e891d6f8ee9632ea240005f2
YuChiChen12 <yuchi11312@gmail.com>
2024-06-26 22:41:17 +0800

    no streamlit extras

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_extras.add_vertical_space import add_vertical_space
 
 
 # Sidebar contents
@@ -13,5 +12,4 @@ with st.sidebar:
     - [OpenAI](https://platform.openai.com/docs/models) LLM model
  
     ''')
-    add_vertical_space(5)
     st.write('Made with ❤️ by [Prompt Engineer](https://youtube.com/@engineerprompt)')
